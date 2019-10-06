a2e1764a2353bed362c1ea528714daf3eabc7152
Cameron Reikes <cameronreikes@gmail.com>
2019-10-05 23:51:22 -0700

    Remove temporary godot settings cache after build

--- bundle.sh
+++ bundle.sh
@@ -9,4 +9,4 @@ mkdir exports/web
 godot-headless --path src --export web "$(readlink -f ./exports/web/index.html)"
 butler push exports/web lajollagamesclub/glitchy-platformer:web 
 
-cp tmp.tres /home/creikey/.config/godot/editor_settings-3.tres
+mv tmp.tres /home/creikey/.config/godot/editor_settings-3.tres
