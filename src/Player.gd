b73341a22bd74e032d22da597d936151fffad937
Cameron Reikes <cameronreikes@gmail.com>
2019-10-05 18:51:45 -0700

    Add comments and jump input event string

--- src/Player.gd
+++ src/Player.gd
@@ -10,11 +10,13 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	# movement vector
 	var move: Vector2 = Vector2()
 	
+	# fetching input
 	move.x = int(Input.is_action_pressed("g_right")) - int(Input.is_action_pressed("g_left"))
 	
-	
+	# tweak animations
 	if abs(move.x) > 0:
 		$AnimatedSprite.play("right")
 	else:
@@ -24,6 +26,7 @@ func _physics_process(delta):
 	else:
 		$AnimatedSprite.flip_h = false
 	
+	# calculate physics
 	velocity += acceleration*delta
 	velocity.x = move.x*move_speed
 	velocity = move_and_slide(velocity, Vector2(0, 1))
