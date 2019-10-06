abb6de47afc2403d5d77a06941ba5e3c9bc321b7
Cameron Reikes <cameronreikes@gmail.com>
2019-10-06 15:07:15 -0700

    Add creative signposts to level

--- src/SignPost.gd
+++ src/SignPost.gd
@@ -1,13 +1,22 @@
+tool
 extends Area2D
 
-func _ready():
-	pass
+export (String) var text = "testing text" setget set_text
 
+func _ready():
+	if not Engine.editor_hint: # hide notices when game starts
+		$MarginContainer.rect_scale = Vector2(0.1, 1)
+		$MarginContainer.modulate = Color("00ffffff")
+
+func set_text(new_text):
+	text = new_text
+	if has_node("MarginContainer/MarginContainer/Label"):
+		$MarginContainer/MarginContainer/Label.text = new_text
 
 func _on_SignPost_body_entered(body):
-	if body.is_in_group("player"):
-		$AnimationPlayer.play("show")
+	if body.is_in_group("player") and not Engine.editor_hint:
+		$AnimationPlayer.play("show") 
 
 func _on_SignPost_body_exited(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not Engine.editor_hint:
 		$AnimationPlayer.play("hide")
