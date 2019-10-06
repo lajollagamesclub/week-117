10f36bba7e7e3264a4776c2afdbd098fc39bb429
Cameron Reikes <cameronreikes@gmail.com>
2019-10-05 20:39:06 -0700

    Cache editor settings before building with headless

--- bundle.sh
+++ bundle.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+cp /home/creikey/.config/godot/editor_settings-3.tres tmp.tres
+
 rm -r exports/web
 mkdir exports/web
 godot-headless --path src --export web "$(readlink -f ./exports/web/index.html)"
 butler push exports/web lajollagamesclub/glitchy-platformer:web 
+
+cp tmp.tres /home/creikey/.config/godot/editor_settings-3.tres
